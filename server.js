af7a7552d3f7066dfc527f669ace7d9a8a25dffb
Anthony Taranto <s3694289@student.rmit.edu.au>
2021-06-08 15:01:31 +1000

    server

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express')
 
 var app = express();
 
-app.use(express.static('./redback-crawler/dist/redback-crawler'));
+app.use(express.static('./orchard-pig-frontend'));
 
 // app.get('/assets', function(req, res){
 //     res.sendFile(__dirname + '/dist/assets');
 // });
 
 app.get('/*', function(req, res){
-    res.sendFile('index.html', {root: './redback-crawler/dist/redback-crawler'}
+    res.sendFile('placeholder.html', {root: './orchard-pig-frontend'}
     );
 });
 
