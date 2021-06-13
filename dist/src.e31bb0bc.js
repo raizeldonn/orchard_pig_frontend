b4d3b53994cc05ea74e6ffea43c73a0b3a982186
Anthony Taranto <s3694289@student.rmit.edu.au>
2021-06-13 15:26:26 +1000

    .

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -15957,7 +15957,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63509" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57160" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
