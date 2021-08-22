865dbe7cc4cbd3cd57ce408ac749a2616a0daa5a
raizel <s3690975@student.rmit.edu.au>
2021-08-22 12:54:39 +0300

    merge

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -11757,7 +11757,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53686" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65046" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
